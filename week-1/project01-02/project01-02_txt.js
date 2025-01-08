f1b11615055ed65175e718b45b74b4abb4d33f11
Noah <nhamblen21@gmail.com>
2025-01-08 15:32:30 -0600

    Fix variable names and begin adding js to page

--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -10,6 +10,6 @@
 
 // define variables for service name and service speed
 // Service Names
-service1Name = "Basic", service2Name = "Express", service1Name = "Extreme", service2Name = "Ultimate";
+service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate";
 // Service Speeds
-service1Speed = "0 Mbps", service2Speed = "100 Mbps", service1Speed = "500 Mbps", service2Speed = "1 Gig";
+service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
