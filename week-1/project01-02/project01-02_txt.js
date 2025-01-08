254208fe8d6954f252e5b20d05cce3660ddcc5f6
Noah <nhamblen21@gmail.com>
2025-01-08 15:25:40 -0600

    Add variables with comments

--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -2,10 +2,14 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Noah Hamblen
+    Date: 1/8/25
 
     Filename: project01-02.js
 */
 
-//define variables for service name and service speed
+// define variables for service name and service speed
+// Service Names
+service1Name = "Basic", service2Name = "Express", service1Name = "Extreme", service2Name = "Ultimate";
+// Service Speeds
+service1Speed = "0 Mbps", service2Speed = "100 Mbps", service1Speed = "500 Mbps", service2Speed = "1 Gig";
