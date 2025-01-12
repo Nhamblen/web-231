8174707d1bf8f6663297c5e2c10ada1a9f225719
Noah <nhamblen21@gmail.com>
2025-01-12 13:53:18 -0600

    Declare variables and display message with fields

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -11,5 +11,9 @@
 
 
       function verifyForm() {
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let phone = document.getElementById("phone").value;
 
+        name && email && phone ? window.alert ("Thank you!") : window.alert ("Please fill in all fields") ;
       }
