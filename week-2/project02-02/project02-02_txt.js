3385894a1287d980af8fd5d889fbf300c04156ab
Noah <nhamblen21@gmail.com>
2025-01-12 14:01:12 -0600

    Format JavaScript with Prettier

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -9,13 +9,14 @@
       Filename: project02-02.js
  */
 
-
 function verifyForm() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
 
-    name && email && phone ? window.alert ("Thank you!") : window.alert ("Please fill in all fields") ;
-    }
+  name && email && phone
+    ? window.alert("Thank you!")
+    : window.alert("Please fill in all fields");
+}
 
 document.getElementById("submit").addEventListener("click", verifyForm);
