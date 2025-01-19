bc359ed0317d70d0ed77d27a852ea6fd382cb0a3
Noah <nhamblen21@gmail.com>
2025-01-19 09:39:15 -0600

    Create constant variables

--- week-3/project02-04/project02-04_txt.js
+++ week-3/project02-04/project02-04_txt.js
@@ -9,6 +9,13 @@
       Filename: project02-04.js
  */
 
+const CHICKEN_PRICE = 10.95;
+const HALIBUT_PRICE = 13.95;
+const BURGER_PRICE = 9.95;
+const SALMON_PRICE = 18.95;
+const SALAD_PRICE = 7.95;
+const SALES_TAX = 0.07;
+
 // Function to display a numeric value as a text string in the format $##.##
 function formatCurrency(value) {
   return "$" + value.toFixed(2);
