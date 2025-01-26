81dd66ff5b7798c163e41071d83dc326bd02cc33
Noah <nhamblen21@gmail.com>
2025-01-26 11:58:19 -0600

    Add calcTotal function and fix class variable

--- week-4/project03-01/project03-01_txt.js
+++ week-4/project03-01/project03-01_txt.js
@@ -9,10 +9,24 @@
       Filename: project03-01.js
 */
 
-let menuItems = getElementByClassName("menuItem");
+let menuItems = document.getElementsByClassName("menuItem");
 
+// Loops through menuItems
 for (let i = 0; i < menuItems.length; i++) {
   menuItems[i].addEventListener("click", calcTotal);
+}
+
+function calcTotal() {
+  let orderTotal = 0;
+  // Loops through menuItems
+  for (let i = 0; i < menuItems.length; i++) {
+    // Code to run if the item is checked
+    if (menuItems[i].checked) {
+      // Increase the orderTotal by the value of the checked item
+      orderTotal += Number(menuItems[i].value);
+    }
+  }
+  document.getElementById("billTotal").innerHTML = formatCurrency(orderTotal);
 }
 
 // Function to display a numeric value as a text string in the format $##.##
