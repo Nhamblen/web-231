4f7567e948a445245862d4ae8a9ad74ff55370d8
Noah <nhamblen21@gmail.com>
2025-02-17 18:30:43 -0600

    Add multiple functions to countdown and check

--- week-7/project05-01/project05-01.js
+++ week-7/project05-01/project05-01.js
@@ -10,6 +10,9 @@
       Filename: project05-01.js
 */
 
+let timeID;
+let questionList = document.querySelectorAll("div#quiz input");
+
 // Constants to set the time given for the quiz in seconds
 // and the correct answers to each quiz question
 const quizTime = 20;
@@ -24,5 +27,60 @@ let overlay = document.getElementById("overlay");
 quizClock.value = quizTime;
 let timeLeft = quizTime;
 
+// Function to check the user's answers in real-time
+function checkAnswer(question, index) {
+  if (question.value === correctAnswers[index]) {
+    question.classList.remove("incorrect");
+  } else {
+    question.classList.add("incorrect");
+  }
+}
+
+// Add event listeners to each question input for real-time checking
+questionList.forEach((question, index) => {
+  question.addEventListener("input", () => checkAnswer(question, index));
+});
+
 // Declare the ID for timed commands
 // and the node list for questions
+
+document.getElementById("startquiz").onclick = function () {
+  document.getElementById("overlay").className = "showquiz"; // Show quiz
+  timeLeft = quizTime; // Reset the time left
+  quizClock.value = timeLeft; // Update timer display
+  timeID = setInterval(countDown, 1000); // Start countdown every 1 second
+};
+
+function countDown() {
+  if (timeLeft === 0) {
+    clearInterval(timeID); // Stop the timer
+
+    let totalCorrect = checkAnswers(); // Check answers
+
+    if (totalCorrect === correctAnswers.length) {
+      alert("Congratulations! You got 100%");
+    } else {
+      alert(
+        `You answered ${totalCorrect} out of ${correctAnswers.length} questions correctly.`
+      );
+    }
+    document.getElementById("overlay").className = "hidequiz";
+  } else {
+    timeLeft--; // Decrease time
+    quizClock.value = timeLeft; // Update timer display
+  }
+}
+
+// Function to check the users answers when the timer ends
+function checkAnswers() {
+  let totalCorrect = 0;
+  questionList.forEach((question, index) => {
+    if (question.value === correctAnswers[index]) {
+      totalCorrect++;
+      question.classList.remove("incorrect");
+    } else {
+      question.classList.add("incorrect");
+    }
+  });
+  return totalCorrect;
+}
